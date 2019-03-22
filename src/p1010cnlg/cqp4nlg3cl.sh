7e16f1679e632f59c360512c4821b41a71fc36ea
Bogdan Babych <bogdan.babych@gmail.com>
2019-03-22 15:37:22 +0000

    s030x development: template database compiled

---
+++ src/p1010cnlg/cqp4nlg3cl.sh
@@ -0,0 +1,2 @@
+perl cqp4nlg3cl.pl --template='WP V.* J.*=;valid,;redeemable,;in-store,;safe IN=:to,:from DT J.* !deal/NN'  > /data/html/corpuslabs/lab201810cnlg/cqp4nlg3cl_out2.txt
+perl cqp4nlg3cl.pl --keyword='deal'  > /data/html/corpuslabs/lab201810cnlg/cqp4nlg3cl_out3.txt
