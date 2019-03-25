8929ae933c08d08bbacaa86368e7e751172fe2a5
Bogdan Babych <bogdan.babych@gmail.com>
2019-03-25 13:02:00 +0000

    s030x development: template database compiled

--- src/p1010cnlg/cqpquery4nlg3cl.pm
+++ src/p1010cnlg/cqpquery4nlg3cl.pm
@@ -924,16 +924,20 @@ sub printLoL{
 }  # end: sub printLoL
 
 
-sub runCollocField2updateDFieldStat2CalculatePositions4NLG{
+sub runCollocField2updateDFieldStatPosition2CalculatePositions4NLG{
     # calculating positions for collocation generation
 
 } # end: runCollocField2updateDFieldStat2CalculatePositions4NLG
 
         
-sub runCollocField2updateDFieldStat4NLG{
+sub runCollocField2updateDFieldStatPosition4NLG{
     # update collocation field for one specific position: 
     # steps: calculate needed positions; 
-    runCollocField2updateDFieldStat2CalculatePositions4NLG();
+    my $iPosition = shift(@_);
+    print STDERR "    runCollocField2updateDFieldStatPosition4NLG :: iPosition = $iPosition \n";
+    
+    
+    runCollocField2updateDFieldStatPosition2CalculatePositions4NLG();
     
     # for each position pair run collocation search, given parameters (separate function).
 
@@ -972,11 +976,11 @@ sub runCollocField4NLG{
     # (\@nlgFilterTemplateXLoLLexProtected, \@nlgFilterTemplateXLoLLex, \@nlgFilterTemplateXPosOnly, \@nlgFilterTemplateXPos, \@nlgFilterTemplateXLofLStop,  \@nlgFilterTemplateXLofLGo )
     
     # execute collocation searches, according to the schedule, and updating the data structure
-    foreach $IPosition (@LSchedule){
+    foreach my $IPosition (@LSchedule){
         # execute collocation search, calculating positions and updating the data structure ; ~ is used for separating scores;
         # list of hashes in the data structure ?
         # 2 steps: 1. updating the data structure for each position
-        runCollocField2updateDFieldStat4NLG();
+        runCollocField2updateDFieldStatPosition4NLG($IPosition);
     
     }
     # 2 steps: 2: at the end: collect top lists, copy to Protected and generate collocation searches;
